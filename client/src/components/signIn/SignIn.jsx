4dce9c5d7ce70a24d82d95eb6eb395a2d42762d4
Kashiff Khan <kashiff.k@yahoo.com>
2020-12-02 10:22:45 -0500

    Login functionality started

--- client/src/components/signIn/SignIn.jsx
+++ client/src/components/signIn/SignIn.jsx
@@ -1,11 +1,48 @@
 import './SignIn.css'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function SignIn() {
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
   return (
     <div>
       <h2 className="signIn-title">My Poetry Collection</h2>
       <form>
-        <input className="signIn-username" placeholder="Username"></input>
+        <label htmlFor="username">
+          <input
+            type='text'
+            className="signIn-username"
+            name='username'
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type='password'
+            className="signIn-password"
+            name='password'
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button>Submit</button>
+        <Link to='register'><button>Register</button></Link>
       </form>
     </div>
   )
