3a5972d736f1476973612425c6cad6a27c882274
Kashiff Khan <kashiff.k@yahoo.com>
2020-12-03 11:00:08 -0500

    Navbar adjustments with ternary

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import './Navbar.css'
 
-export default function Navbar() {
+export default function Navbar(props) {
+  
+  const { currentUser } = props;
+
   return (
     <div className="navbarFullDiv">
+      {currentUser ? <p>Welcome, {currentUser.first_name} {currentUser.last_name}</p> : <p>no</p>}
     </div>
   )
 }
