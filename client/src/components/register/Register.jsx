0d5531a5d5b2cdcaff8bb21f3a1121ee7e69851f
Kashiff Khan <kashiff.k@yahoo.com>
2020-12-04 09:40:12 -0500

    Register page style complete.

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -24,87 +24,83 @@ export default function Register(props) {
   return (
     <div className='register_div'>
       <div className='register_interior_div'>
-      <h2 className="register_title">Register</h2>
         <form
           className="register_form"
           onSubmit={(e) => {
             e.preventDefault();
             props.handleRegister(formData);
-        }}>
-        <label htmlFor="First Name">First Name
-          <input
-            type='text'
-            className="registerFirstName"
-            name='first_name'
-            value={formData.first_name}
-            onChange={handleChange}
-          />
-        </label>
-        <br/>
-        <label htmlFor="Last Name">Last Name
-          <input
-            type='text'
-            className="registerLastName"
-            name='last_name'
-            value={formData.last_name}
-            onChange={handleChange}
-          />
-        </label>
-        <br/>
-        <label htmlFor="date-of-birth">Date of Birth
-          <input
-            type='date'
-            className="registerDOB"
-            name='date_of_birth'
-            value={formData.date_of_birth}
-            onChange={handleChange}
-          />
-        </label>
-        <br/>
-        <label htmlFor="email">Email
-          <input
-            type='email'
-            className="registerEmail"
-            name='email'
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <label htmlFor="phone-number">Phone Number
-          <input
-            type='tel'
-            className="registerPhone"
-            name='phone_number'
-            placeholder="xxx-xxx-xxxx"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            value={formData.phone_number}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <label htmlFor="username">Username
-          <input
-            type='text'
-            className="registerUsername"
-            name='username'
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <label htmlFor="password">Password
-          <input
-            type='password'
-            className="registerPassword"
-            name='password'
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </label>
-        <br/>
-        <button>Submit</button>
-      </form>
+          }}>
+          <span className="register_title">Registration Page</span>
+          <label htmlFor="First Name"><span className="register_labels">First Name</span>
+            <input
+              type='text'
+              className="register_inputs"
+              name='first_name'
+              value={formData.first_name}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="Last Name"><span className="register_labels">Last Name</span>
+            <input
+              type='text'
+              className="register_inputs"
+              name='last_name'
+              value={formData.last_name}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="date-of-birth"><span className="register_labels">Date of Birth</span>
+            <input
+              type='date'
+              className="register_inputs"
+              name='date_of_birth'
+              value={formData.date_of_birth}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="email"><span className="register_labels">Email</span>
+            <input
+              type='email'
+              className="register_inputs"
+              name='email'
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="phone-number"><span className="register_labels">Phone Number</span>
+            <input
+              type='tel'
+              className="register_inputs"
+              name='phone_number'
+              placeholder="xxx-xxx-xxxx"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              value={formData.phone_number}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="username"><span className="register_labels">Username</span>
+            <input
+              type='text'
+              className="register_inputs"
+              name='username'
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="password"><span className="register_labels">Password</span>
+            <input
+              type='password'
+              className="register_inputs"
+              name='password'
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </label>
+          <div>
+            <button className="register_submit_button">Submit</button>
+            <button className="register_reset_button" type="reset">Reset</button>
+          </div>
+        </form>
       </div>
     </div>
   )
