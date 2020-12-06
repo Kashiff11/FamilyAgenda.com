3e98a8a30ebcf377c9b8c1e2a5e3516297ef1abb
Kashiff Khan <kashiff.k@yahoo.com>
2020-12-06 17:32:06 -0500

    One Poet functionality in place.

--- client/src/components/poet/Poet.jsx
+++ client/src/components/poet/Poet.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
+import { Link, useParams } from 'react-router-dom'
+import { getOnePoet } from '../../services/poets';
+
 import "./Poet.css"
 
  export default function Poet(props) {
-  return (
+  
+  const [poetData, setPoetData] = useState({
+    first_name: '',
+    last_name: '',
+    year_born: null,
+    year_died: null,
+    birthplace: '',
+    short_bio: ''
+  }) 
+  
+  const { id } = useParams();
+   const { currentUser } = props;
+   
+   useEffect(() => {
+    const fetchOnePoet = async () => {
+      const poetData = await getOnePoet(id);
+      setPoetData(poetData);
+    }
+    fetchOnePoet(id);
+  }, [])
+   
+   return (
     <div className="poet_container">
-      <h1>Poet Page</h1>
+       <h1>{poetData.first_name}</h1>
+
+
     </div>
   );
 }
